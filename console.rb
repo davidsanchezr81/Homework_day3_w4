706a58b2f49ca2d31205f8f1c0e65a788a2a4b02
David <davidsanchezr81@gmail.com>
2018-02-21 21:55:55 +0000

    Starting Part 4

---
+++ console.rb
@@ -0,0 +1,14 @@
+require_relative('models/student.rb')
+
+
+
+require('pry-byebug')
+
+student1 = Student.new({ 'first_name' => 'Pedro', 'second_name' => 'Galvez', 'house' => 'Quinta5', 'age' => 24})
+student1.save()
+
+student2 = Student.new({ 'first_name' => 'John', 'second_name' => 'Schwartz', 'house' => 'Quinta10', 'age' => 58})
+student2.save()
+
+binding.pry
+nil
---
+++ models/student.rb
@@ -0,0 +1,52 @@
+require_relative('../db/sql_runner')
+
+
+
+class Student
+
+  attr_reader :first_name, :second_name, :house, :age
+
+  def initialize (options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @second_name = options['second_name']
+    @house = options['house']
+    @age = options['age'].to_i
+  end
+
+  #
+  def save()
+    sql = "INSERT INTO students
+    (
+      first_name,
+      second_name,
+      house,
+      age
+    )
+    VALUES
+    (
+      $1, $2, $3, $4
+    )
+    RETURNING *"
+    values = [@first_name, @second_name, @house, @age]
+    student = SqlRunner.run(sql, values).first
+    @id = student['id'].to_i
+  end
+
+  def Student.all
+    sql = "SELECT * FROM students"
+    values = []
+    students = SqlRunner.run(sql, values)
+    result = students.map { |student| Student.new( student) } # esto se cancelan porque usamos el metodo de Location.map_items
+    return result
+  end
+
+  def Student.find( id )
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    student = SqlRunner.run( sql, values )
+    result = Student.new( student.first )
+    return result
+  end
+
+end
